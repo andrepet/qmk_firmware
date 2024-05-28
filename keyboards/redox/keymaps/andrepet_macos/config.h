5b3e9a5e7a68301eb2823e93880b3287974e2166
Andreas Pettersson <andreas.pettersson@solidbeans.com>
2024-05-28 14:40:51 +0200

    Update mouse movement to be same as crkbd

--- keyboards/redox/keymaps/andrepet_macos/config.h
+++ keyboards/redox/keymaps/andrepet_macos/config.h
@@ -28,6 +28,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 175
 
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      3
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
 // remove debug to save filesize
 #ifndef NO_DEBUG
 #define NO_DEBUG
