d5209bfdcdcd0f7547c1de9d8d2a256b26082664
Andreas Pettersson <andreas.pettersson@sj.se>
2024-12-24 01:40:19 +0100

    Enable mouse in rules.mk, cleanup mouse configuration

--- keyboards/crkbd/keymaps/andrepet_left_swedish/rules.mk
+++ keyboards/crkbd/keymaps/andrepet_left_swedish/rules.mk
@@ -5,6 +5,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no		    # Enable keyboard RGB underglow
 RGB_MATRIX_ENABLE = yes			# Enable per-key RGB
 OLED_DRIVER_ENABLE = yes 		# Enable check for oled at all for each press
+MOUSEKEY_ENABLE = yes
 
 # to save filesize
 LTO_ENABLE = yes #  Enables Link Time Optimization (LTO) when compiling the keyboard. This makes the process take longer, but it can significantly reduce the compiled size (and since the firmware is small, the added time is not noticeable).
